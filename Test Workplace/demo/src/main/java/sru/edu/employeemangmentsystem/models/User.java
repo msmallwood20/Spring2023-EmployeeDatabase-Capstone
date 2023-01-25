eea555072edbc0573a8881bbbaef5970ee7642fb
Maya Smallwood <mns1013@sru.edu>
2023-01-25 14:02:20 -0500

    Initial Commit 1.25.23

---
+++ Test Workplace/demo/src/main/java/sru/edu/employeemangmentsystem/models/User.java
@@ -0,0 +1,15 @@
+package sru.edu.employeemangmentsystem.models;
+
+public class User {
+	
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
