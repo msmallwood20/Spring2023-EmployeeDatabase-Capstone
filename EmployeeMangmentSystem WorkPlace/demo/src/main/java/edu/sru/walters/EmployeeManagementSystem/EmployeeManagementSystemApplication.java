23a91f5049d26d84c06a24de34f787e5f074e5a4
Bradon D Walters <bdw1006@sru.edu>
2023-02-04 19:50:10 -0500

    Security Commit

--- EmployeeMangmentSystem WorkPlace/demo/src/main/java/edu/sru/walters/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ EmployeeMangmentSystem WorkPlace/demo/src/main/java/edu/sru/walters/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -2,9 +2,17 @@ package edu.sru.walters.EmployeeManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class EmployeeManagementSystemApplication {
+@ComponentScan("module-service")
+public class EmployeeManagementSystemApplication extends SpringBootServletInitializer {
+	
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmployeeManagementSystemApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
