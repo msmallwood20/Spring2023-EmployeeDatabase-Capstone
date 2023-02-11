431cdbaa58fb8abf530b4775460e85681274ad99
Maya Smallwood <mayasmallwood41@gmail.com>
2023-02-11 16:24:02 -0500

    added controller tag to MainController class

--- EmployeeMangmentSystem WorkPlace/demo/src/main/java/edu/sru/walters/EmployeeManagementSystem/controllers/MainController.java
+++ EmployeeMangmentSystem WorkPlace/demo/src/main/java/edu/sru/walters/EmployeeManagementSystem/controllers/MainController.java
@@ -3,6 +3,7 @@ package edu.sru.walters.EmployeeManagementSystem.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 		@GetMapping("/HomePage")
 		public String home() {
