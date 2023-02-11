b168b841ed278a2a5e437b3a74a318e39311d84c
Maya Smallwood <mayasmallwood41@gmail.com>
2023-02-11 08:39:22 -0500

    Setting GetMapping to go to the landing page

--- EmployeeMangmentSystem WorkPlace/demo/src/main/java/edu/sru/walters/EmployeeManagementSystem/controllers/MainController.java
+++ EmployeeMangmentSystem WorkPlace/demo/src/main/java/edu/sru/walters/EmployeeManagementSystem/controllers/MainController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class MainController {
-		@GetMapping("/login")
-		public String login() {
-			return "login";
+		@GetMapping("/HomePage")
+		public String home() {
+			return "HomePage";
 		}
 }
