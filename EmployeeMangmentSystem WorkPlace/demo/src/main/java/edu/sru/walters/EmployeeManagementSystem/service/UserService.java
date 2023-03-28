bdb5d7ae50ebc2b2e450edee7e32fc6497e57404
Maya Smallwood <122891139+mns1013@users.noreply.github.com>
2023-03-28 09:24:15 -0400

    Delete EmployeeMangmentSystem WorkPlace directory
    Removing Old Project Versions

--- EmployeeMangmentSystem WorkPlace/demo/src/main/java/edu/sru/walters/EmployeeManagementSystem/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package edu.sru.walters.EmployeeManagementSystem.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-import edu.sru.walters.EmployeeManagementSystem.models.Users;
-
-public interface UserService extends UserDetailsService {
-	
-
-}
